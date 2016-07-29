fa4416fef09cf687ce69ee084ce839c7297a8450
Bebeau <kyle@theinitgroup.com>
2016-07-29 15:36:07 -0700

    show media tab

--- functions.php
+++ functions.php
@@ -52,7 +52,7 @@ function remove_menus(){
 	// remove_menu_page( 'themes.php' );
 	remove_menu_page( 'plugins.php' );
 	// remove_menu_page( 'users.php' );
-	remove_menu_page( 'upload.php' );
+	// remove_menu_page( 'upload.php' );
 }
 add_action( 'admin_menu', 'remove_menus' );
 
