199d966a7c0b68b7e0cb81c958105b6e4c574d22
Bebeau <kyle@theinitgroup.com>
2016-08-01 15:16:36 -0700

    layout basic 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				Ops, something went wrong.
-			</div>
+	
+	<div class="outer">
+		<div class="inner">
+			<h1>Ops, something went wrong.</h1>
+			<a class="contact-btn" href="<?php echo site_url();?>">Go Back</a>
 		</div>
 	</div>
 
